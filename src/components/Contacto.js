63fffcca7e40c76a30fb34a6088327b7f8994e48
Mamen <mcariasdeharo@gmail.com>
2022-07-09 01:06:09 +0200

    Página de contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import { Footer } from './layout/Footer'
+import React, { useState } from 'react'
+import linkedin from '../media/linkedin.png'
+import github from '../media/github.png'
+import trailhead from '../media/trailhead.png'
+import { useSpring, animated } from 'react-spring'
+import emailjs from 'emailjs-com'
 
 export const Contacto = () => {
+
+  const [send, setSend] = useState(false);
+  const [notSend, setNotSend] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ii6y1ag', 'template_m1t0e1o', e.target, 'Oe7F0bgP9hxvmaflV')
+      .then((result) =>{
+        console.log(result.text);
+        setSend(true);
+      }, (error) => {
+        console.log(error.text);
+        setNotSend(true);
+      });
+  }
+
+  //Animación para que el formulario aparezca desde arriba
+  const fallText = useSpring({
+    to: {margin: 10},
+    from: {margin: -150}
+  })
+
   return (
-    <div>
-      contacto
+    <div className='contact'>
+      <animated.h2 className='contact-text' style={fallText}>¡Contacta conmigo!</animated.h2>
+
+      <form className='contact-form' onSubmit={sendEmail}>
+        <input type='text' placeholder='Nombre y apellidos' name='name' required='true'/>
+        <input type='email' placeholder='Email' name='email' required='true'/>
+        <textarea placeholder='Deja aquí tu mensaje...' name='message'/>
+        <p className={send ? 'message-active' : 'message-hidden'}>Mensaje enviado correctamente</p>
+        <p className={notSend ? 'message2-active' : 'message2-hidden'}>Ha habido un error al enviar el mensaje.Inténtelo más tarde, por favor</p>
+        <input type='submit' value='ENVIAR' />
+      </form>
+      
+
+      <div className='social-networks'>
+          <a href='https://www.linkedin.com/in/mcariasdeharo/' target='_blank' rel='noreferrer'><img className='social-icons' src={linkedin} alt='LinkedIn'></img></a>
+          <a href='https://github.com/mamenArias' target='_blank' rel='noreferrer'><img className='social-icons' src={github} alt='GitHub'></img></a>
+          <a href='https://trailblazer.me/id/mcariasdeharo' target='_blank' rel='noreferrer'><img className='social-icons' src={trailhead} alt='Trailblazer'></img></a>
+      </div>
     </div>
   )
 }
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -20,11 +20,11 @@ export const Inicio = () => {
       <div className='introduction'>
             <animated.p className='personal-text' style={{opacity, transition: '500ms'}}><strong>¡Buenas!</strong><br/><br/>
               Mi nombre es Mª Carmen Arias, aunque me llaman Mamen, y soy  
-              <br/><strong> Técnico Superior de Desarrollo de Aplicaciones Multiplataforma. </strong>
+              <br/><strong className='text-strong'> Técnico Superior de Desarrollo de Aplicaciones Multiplataforma. </strong>
               <br/><br/>Estudié el Grado de Fisioterapia en 2009 y el Máster en Osteopatía en 2013, y he trabajado como fisioterapeuta
               7 años de mi vida. Sin embargo, a raíz de la pandemia y por una serie de circunstancias, decidí darle un giro de 180º a mi vida
-              y estudiar algo que siempre me había gustado desde pequeñita: <strong> Informática. </strong><br/><br/>
-              Me apunté al CFGS de <strong> Desarrollo de Aplicaciones Multiplataforma</strong> y puedo decir que fue la mejor decisión que tomé.
+              y estudiar algo que siempre me había gustado desde pequeñita: <strong className='text-strong'> Informática. </strong><br/><br/>
+              Me apunté al CFGS de <strong className='text-strong'> Desarrollo de Aplicaciones Multiplataforma</strong> y puedo decir que fue la mejor decisión que tomé.
               Quiero trabajar en el mundo del Desarrollo y, por supuesto, no pienso dejar de formarme, ya que creo que hay un mundo enorme por descubrir
               dentro de la programación.
             </animated.p>

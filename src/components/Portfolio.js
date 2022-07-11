4293949e35dcbf7de942781b289b258127fffa82
Mamen <mcariasdeharo@gmail.com>
2022-07-11 14:43:17 +0200

    Añadidos más proyectos personales

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,6 +12,23 @@ import menu from '../media/proyectos/hit-the-cooks/menu.jpeg'
 import alimentos from '../media/proyectos/hit-the-cooks/alimentos.jpeg'
 import alimentos2 from '../media/proyectos/hit-the-cooks/alimentos2.jpeg'
 import nosotros from '../media/proyectos/hit-the-cooks/nosotros.jpeg'
+import menup from '../media/proyectos/plucky/menu.jpg'
+import controles from '../media/proyectos/plucky/controles.jpg'
+import escenario1 from '../media/proyectos/plucky/inicio.jpg'
+import gameover from '../media/proyectos/plucky/gameover.jpg'
+import enemigo1 from '../media/proyectos/plucky/enemies1.jpg'
+import puerta from '../media/proyectos/plucky/door.jpg'
+import escenario2 from '../media/proyectos/plucky/escenario2.jpg'
+import enemigo2 from '../media/proyectos/plucky/enemies2.jpg'
+import escenario3 from '../media/proyectos/plucky/escenario3.jpg'
+import jefe from '../media/proyectos/plucky/jefe.jpg'
+import jefecaida from '../media/proyectos/plucky/jefecaida.jpg'
+import finjuego from '../media/proyectos/plucky/fin.jpg'
+
+
+
+
+
 
 export const Portfolio = () => {
     return (
@@ -67,7 +84,9 @@ export const Portfolio = () => {
         </article>
 
         <article className='proyect-item'>
-          <ReactPlayer className='carousel-large' url='https://www.youtube.com/watch?v=Tk7HJgGEZAY' loop={true} muted={true}/>
+          <div className='carousel-large'>
+            <ReactPlayer width='100%' height='100%' url='https://www.youtube.com/watch?v=6FTLyHbB1kI' loop={true} muted={true}/>
+          </div>
           <div className='text'>
             <h3>{'>'} Remember</h3>
             <p>Se trata de un juego realizado con <strong className='text-strong'>Unity</strong> y <strong className='text-strong'>C# </strong>
@@ -109,6 +128,68 @@ export const Portfolio = () => {
             <p>Con esta aplicación podemos crear una lista de la compra y compartirla con otros usuarios, pudiendo tener varias listas compartidas a la vez.
               Además, cuenta con web-scrapping del supermercado Día, para conocer a qué precio están los productos en dicha web.
             </p>
+          </div>
+        </article>
+
+        <article className='proyect-item'>
+          <Carousel className='carousel-large' autoPlay interval={5000} infiniteLoop showThumbs={false}>
+            <div className='image-car'>
+              <img src={menup} alt='Menú inicial del juego'></img>
+            </div>
+            <div className='image-car'>
+              <img src={controles} alt='Controles'></img>
+            </div>
+            <div className='image-car'>
+              <img src={escenario1} alt='Prinmer escenario'></img>
+            </div>
+            <div className='image-car'>
+              <img src={enemigo1} alt='Un tipo de enemigo'></img>
+            </div>
+            <div className='image-car'>
+              <img src={gameover} alt='Pantalla Game Over'></img>
+            </div>
+            <div className='image-car'>
+              <img src={puerta} alt='Final Pantalla'></img>
+            </div>
+            <div className='image-car'>
+              <img src={escenario2} alt='Segundo escenario'></img>
+            </div>
+            <div className='image-car'>
+              <img src={enemigo2} alt='Tipo de enemigo'></img>
+            </div>
+            <div className='image-car'>
+              <img src={escenario3} alt='Tercer escenario'></img>
+            </div>
+            <div className='image-car'>
+              <img src={jefe} alt='Jefe final'></img>
+            </div>
+            <div className='image-car'>
+              <img src={jefecaida} alt='Jefe final derrotado'></img>
+            </div>
+            <div className='image-car'>
+              <img src={finjuego} alt='Pantalla fin del juego'></img>
+            </div>
+          </Carousel>
+          <div className='text'>
+            <h3>{'>'} Plucky!</h3>
+            <p>Para la asignatura de 'Programación Multimedia y Dispositivos Móviles' de 2º DAM, tuvimos que realizar un juego con <strong className='text-strong'>Unity</strong> y <strong className='text-strong'>C#</strong>.
+            </p>
+            <p>Se trata de un juego estilo plataformas, donde el objetivo es conseguir todas las monedas para completar la pantalla y pasar a la siguiente.
+            Todos los assets son de itch.io, habiendo realizando yo la programación del juego, el diseño de niveles y los menús.
+            </p>
+          </div>
+        </article>
+
+        <article className='proyect-item'>
+          <div className='carousel-large'>
+            <ReactPlayer width='100%' height='100%' url='https://www.youtube.com/watch?v=VJ-MNQQp0hU' autoPlay={true} loop={true} muted={true}/>
+          </div>
+          <div className='text'>
+            <h3>{'>'} Freeling</h3>
+            <p>Esta aplicación de <strong className='text-strong'>Android</strong> fue mi proyecto final del CFGS, que realicé con mi compañero <a href='https://miguelangelarcos.com/' target='_blank' rel='noreferrer'>Miguel Ángel Arcos Reyes</a>.</p>
+            <p>Se trata de una app para conocer gente, que funciona a través de <strong className='text-strong'>Google Nearby</strong>, localizando a personas con las que te cruzas que también tengan la app.
+              Tiene un sistema de logros, de medallas para ver si la persona es agradable o no, y un chat individual con quien elijas.</p>
+            <p>Para este proyecto hemos trabajado con <strong className='text-strong'>Kotlin</strong>, <strong className='text-strong'>Firebase</strong> y <strong className='text-strong'>GitHub</strong>.</p>
           </div>
         </article>
         
